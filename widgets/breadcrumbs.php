26b5bbd47c01f0e42f6c3a3eedef3913627a1b76
malles <matthijs@bixie.nl>
2015-12-11 01:01:09 +0100

    no app needed

--- widgets/breadcrumbs.php
+++ widgets/breadcrumbs.php
@@ -6,16 +6,7 @@ return [
 
     'label' => 'Breadcrumbs',
 
-	'events' => [
-
-		'view.scripts' => function ($event, $scripts) use ($app) {
-			$scripts->register('widget-breadcrumbs', 'bixie/breadcrumbs:app/bundle/widget-breadcrumbs.js', ['~widgets']);
-		}
-
-	],
-
     'render' => function ($widget) use ($app) {
-
 
 		return $app['view']('bixie/breadcrumbs/widget-breadcrumbs.php');
     }
